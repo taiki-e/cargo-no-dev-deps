f084af57b62bb4fbf494e6454e7f0c583588f0e1
Taiki Endo <te316e89@gmail.com>
2025-01-15 02:34:43 +0900

    Update rust-lang/rust link

--- src/cli.rs
+++ src/cli.rs
@@ -34,7 +34,7 @@ impl Args {
         const SUBCMD: &str = "no-dev-deps";
 
         // rustc/cargo args must be valid Unicode
-        // https://github.com/rust-lang/rust/blob/1.80.0/compiler/rustc_driver_impl/src/args.rs#L121
+        // https://github.com/rust-lang/rust/blob/1.84.0/compiler/rustc_driver_impl/src/args.rs#L121
         // TODO: https://github.com/rust-lang/cargo/pull/11118
         fn handle_args(
             args: impl IntoIterator<Item = impl Into<OsString>>,
